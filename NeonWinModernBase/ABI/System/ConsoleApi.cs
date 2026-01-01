93f4de25123cd2ac2637af7a7dee18257915f431
MinecraftXinYi <3077501050@qq.com>
2026-01-01 15:44:35 +0800

    [v0.10.1.0]

---
+++ NeonWinModernBase/ABI/System/ConsoleApi.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NeonWindows.ABI.System;
+
+public static class ConsoleApi
+{
+    /// <summary>
+    /// 将调用进程附加到指定进程的控制台作为客户端应用程序。
+    /// </summary>
+    /// <param name="dwProcessId">要使用的控制台的进程标识符。</param>
+    /// <returns>如果该函数成功，则返回值为非零值。 如果函数失败，则返回值为零。 要获得更多的错误信息，请调用 GetLastError。</returns>
+    public static bool AttachConsole(uint dwProcessId)
+    {
+        try
+        {
+            [DllImport(Win32DllName.KernelBase, ExactSpelling = true, SetLastError = true)]
+            [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+            static extern int AttachConsole(uint dwProcessId);
+            return AttachConsole(dwProcessId) != 0;
+        }
+        catch (TypeLoadException)
+        {
+            [DllImport(Win32DllName.Kernel32, ExactSpelling = true, SetLastError = true)]
+            [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+            static extern int AttachConsole(uint dwProcessId);
+            return AttachConsole(dwProcessId) != 0;
+        }
+    }
+
+    /// <summary>
+    /// 使用当前进程的父级的控制台。
+    /// </summary>
+    public const uint ATTACH_PARENT_PROCESS = unchecked((uint)-1);
+
+    /// <summary>
+    /// 为调用进程分配一个新的控制台。
+    /// </summary>
+    /// <returns>如果该函数成功，则返回值为非零值。 如果函数失败，则返回值为零。 若要获取扩展的错误信息，请调用 GetLastError。</returns>
+    public static bool AllocConsole()
+    {
+        try
+        {
+            [DllImport(Win32DllName.KernelBase, ExactSpelling = true, SetLastError = true)]
+            [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+            static extern int AllocConsole();
+            return AllocConsole() != 0;
+        }
+        catch (TypeLoadException)
+        {
+            [DllImport(Win32DllName.Kernel32, ExactSpelling = true, SetLastError = true)]
+            [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+            static extern int AllocConsole();
+            return AllocConsole() != 0;
+        }
+    }
+
+    /// <summary>
+    /// 从其控制台分离调用进程。
+    /// </summary>
+    /// <returns>如果该函数成功，则返回值为非零值。 如果函数失败，则返回值为零。 要获得更多的错误信息，请调用 GetLastError。</returns>
+    public static bool FreeConsole()
+    {
+        try
+        {
+            [DllImport(Win32DllName.KernelBase, ExactSpelling = true, SetLastError = true)]
+            [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+            static extern int FreeConsole();
+            return FreeConsole() != 0;
+        }
+        catch (TypeLoadException)
+        {
+            [DllImport(Win32DllName.Kernel32, ExactSpelling = true, SetLastError = true)]
+            [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+            static extern int FreeConsole();
+            return FreeConsole() != 0;
+        }
+    }
+}
---
+++ NeonWinModernBase/System/Win32Console.cs
@@ -0,0 +1,35 @@
+﻿using NeonWindows.ABI.System;
+
+namespace NeonWindows.System;
+
+/// <summary>
+/// 管理应用程序的 Windows 控制台。
+/// </summary>
+public static class Win32Console
+{
+    /// <summary>
+    /// 为当前进程分配控制台。
+    /// </summary>
+    /// <returns>指示操作是否成功。</returns>
+    public static bool Initialize()
+    {
+        if (!ConsoleApi.AttachConsole(ConsoleApi.ATTACH_PARENT_PROCESS))
+            return ConsoleApi.AllocConsole();
+        return true;
+    }
+
+    /// <summary>
+    /// 为当前进程分配控制台。
+    /// </summary>
+    /// <param name="pid">要使用的控制台的进程标识符。</param>
+    /// <returns>指示操作是否成功。</returns>
+    public static bool Initialize(uint pid)
+        => ConsoleApi.AttachConsole(pid);
+
+    /// <summary>
+    /// 从控制台分离当前进程。
+    /// </summary>
+    /// <returns>指示操作是否成功。</returns>
+    public static bool Unload()
+        => ConsoleApi.FreeConsole();
+}
