ebe9d8a0d753e0608b84bb5c799cea5b5955fe3e
MinecraftXinYi <3077501050@qq.com>
2026-02-16 17:51:40 +0800

    {v0.0.12.0}

---
+++ NeonWinModernCore/UI/Modern/Xaml/XamlAppActivation.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+
+namespace NeonWindows.UI.Modern.Xaml;
+
+public static class XamlAppActivation
+{
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "OnActivated")]
+    private static extern void OnActivatedMethod(Application app, IActivatedEventArgs args);
+
+    public static void InvokeActivationMethod(this Application app, IActivatedEventArgs args)
+        => OnActivatedMethod(app, args);
+}
