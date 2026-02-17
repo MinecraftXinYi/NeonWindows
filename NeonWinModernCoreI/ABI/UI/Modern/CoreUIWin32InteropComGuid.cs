fcfb2cad53990606a5020a01c66adab96a4f81e5
MinecraftXinYi <3077501050@qq.com>
2026-02-17 15:29:39 +0800

    <v0.11.1.0> {v0.0.15.0}

--- NeonWinModernCoreI/ABI/UI/Modern/CoreUIWin32InteropComGuid.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NeonWindows.ABI.UI.Modern;
-
-public static class CoreUIWin32InteropComGuid
-{
-    public const string
-        IID_IInitializeWithWindow = "3E68D4BD-7135-4D10-8018-9FB6D9F33FA1";
-}
