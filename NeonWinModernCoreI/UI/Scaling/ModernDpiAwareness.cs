a91d2848548270b00f8b1f5dd7baacaf66f61a7f
MinecraftXinYi <3077501050@qq.com>
2026-04-19 00:01:23 +0800

    [v0.16.0.1]

---
+++ NeonWinModernCoreI/UI/Scaling/ModernDpiAwareness.cs
@@ -0,0 +1,30 @@
+﻿namespace NeonWindows.UI.Scaling;
+
+public static class ModernDpiAwareness
+{
+    public static bool SetProcessPerMonitorDpiAware(out bool isPerMonitorV2)
+    {
+        isPerMonitorV2 = false;
+        if (AppDpiAwareness.SetCurrentProcessDpiAwarenessMode(DpiAwarenessMode.PerMonitorV2))
+        {
+            isPerMonitorV2 = true;
+            return true;
+        }
+        if (AppDpiAwareness.SetCurrentProcessDpiAwarenessMode(DpiAwarenessMode.PerMonitor))
+            return true;
+        return false;
+    }
+
+    public static bool SetThreadPerMonitorDpiAware(out bool isPerMonitorV2)
+    {
+        isPerMonitorV2 = false;
+        if (AppDpiAwareness2.SetCurrentThreadDpiAwarenessMode(DpiAwarenessMode.PerMonitorV2))
+        {
+            isPerMonitorV2 = true;
+            return true;
+        }
+        if (AppDpiAwareness2.SetCurrentThreadDpiAwarenessMode(DpiAwarenessMode.PerMonitor))
+            return true;
+        return false;
+    }
+}
