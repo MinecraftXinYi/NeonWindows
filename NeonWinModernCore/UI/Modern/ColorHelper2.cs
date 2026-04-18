e1bcc1bf8837931995322e44da5ec9e9358d93c5
MinecraftXinYi <3077501050@qq.com>
2026-04-19 00:02:59 +0800

    [v0.16.0.1] Part2

---
+++ NeonWinModernCore/UI/Modern/ColorHelper2.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI;
+
+namespace NeonWindows.UI.Modern;
+
+/// <summary>
+/// 提供用于处理 Color 值的静态帮助方法。
+/// </summary>
+public static class ColorHelper2
+{
+    /// <summary>
+    /// 检测颜色是否可以归类为浅色。
+    /// </summary>
+    /// <param name="color">要检测的颜色。</param>
+    /// <returns>是否可以归类为浅色。</returns>
+    public static bool IsColorLight(this Color color)
+        => ((5 * color.G) + (2 * color.R) + color.B) > (8 * 128);
+}
