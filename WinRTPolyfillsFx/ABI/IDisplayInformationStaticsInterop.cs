b2b9d381ac222cd7ebb1e5a6c722b2c352600805
MinecraftXinYi <3077501050@qq.com>
2026-05-16 02:32:41 +0800

    [WRPF-v0.2.2]

---
+++ WinRTPolyfillsFx/ABI/IDisplayInformationStaticsInterop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Windows.Win32.Foundation;
+using Windows.Win32.Graphics.Gdi;
+
+namespace NeonWindows.ABI;
+
+[ComImport]
+[InterfaceType(ComInterfaceType.InterfaceIsIInspectable)]
+[Guid("7449121C-382B-4705-8DA7-A795BA482013")]
+internal unsafe interface IDisplayInformationStaticsInterop
+{
+    [return: MarshalAs(UnmanagedType.IUnknown)]
+    object GetForWindow(HWND window, Guid* riid);
+
+    [return: MarshalAs(UnmanagedType.IUnknown)]
+    object GetForMonitor(HMONITOR monitor, Guid* riid);
+}
---
+++ WinRTPolyfillsFx/Graphics/Display/DisplayInformationInterop.cs
@@ -0,0 +1,25 @@
+﻿using NeonWindows.ABI;
+using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Graphics.Display;
+
+namespace NeonWindows.Graphics.Display;
+
+public unsafe static class DisplayInformationInterop
+{
+    public static DisplayInformation GetForWindow(nint window)
+    {
+        Guid iid = IID_IDisplayInformation;
+        return (DisplayInformation)displayInformationInterop.GetForWindow(new(window), &iid);
+    }
+
+    public static DisplayInformation GetForMonitor(nint monitor)
+    {
+        Guid iid = IID_IDisplayInformation;
+        return (DisplayInformation)displayInformationInterop.GetForMonitor(new(monitor), &iid);
+    }
+
+    internal static readonly Guid IID_IDisplayInformation = new(3201372846u, 44483, 19913, 174, 101, 133, 31, 77, 125, 71, 153);
+
+    private static IDisplayInformationStaticsInterop displayInformationInterop = (IDisplayInformationStaticsInterop)WindowsRuntimeMarshal.GetActivationFactory(typeof(DisplayInformation));
+}
